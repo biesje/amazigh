0cf851c1a3e7297c03a37b39c2d382ce2faeb946
Tim <biestim7@gmailcom>
2016-06-22 08:43:14 +0200

    update

---
+++ app/src/main/java/com/example/timbiezeveld/amazighproj/ContentOefen.java
@@ -0,0 +1,41 @@
+package com.example.timbiezeveld.amazighproj;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+
+public class ContentOefen extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.content_oefenen, container, false);
+
+        TextView tv = (TextView) v.findViewById(R.id.tvFragThird);
+        ImageView iv = (ImageView) v.findViewById(R.id.image);
+
+        tv.setText(getArguments().getString("msg"));
+        
+
+        return v;
+    }
+
+    public static ContentOefen newInstance(String text) {
+
+        ContentOefen f = new ContentOefen();
+        Bundle b = new Bundle();
+        b.putString("msg", text);
+
+        f.setArguments(b);
+
+        return f;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/timbiezeveld/amazighproj/oefenen.java
@@ -0,0 +1,50 @@
+package com.example.timbiezeveld.amazighproj;
+
+import android.support.v4.app.Fragment;
+
+
+        import android.os.Bundle;
+        import android.support.v4.app.Fragment;
+        import android.support.v4.app.FragmentActivity;
+        import android.support.v4.app.FragmentManager;
+        import android.support.v4.app.FragmentPagerAdapter;
+        import android.support.v4.view.ViewPager;
+
+
+public class oefenen extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oefenen);
+
+        ViewPager pager = (ViewPager) findViewById(R.id.viewPager);
+        pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+    }
+
+    private class MyPagerAdapter extends FragmentPagerAdapter {
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int pos) {
+            switch(pos) {
+
+                case 0: return ContentOefen.newInstance("Paard");
+                case 1: return ContentOefen.newInstance("Ezel");
+                case 2: return ContentOefen.newInstance("Koe");
+                case 3: return ContentOefen.newInstance("Koe");
+
+
+                default: return ContentOefen.newInstance("ThirdFragment, Default");
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 5;
+        }
+    }
+}
