ed1a0717a86cdb5dccd1d3270e934ec53cd07325
biesje <biestim7@gmail.com>
2016-06-28 16:35:11 +0200

    eten, nog testen

--- app/src/main/java/com/example/timbiezeveld/amazighproj/OefenEten.java
+++ app/src/main/java/com/example/timbiezeveld/amazighproj/OefenEten.java
@@ -15,30 +15,42 @@ public class OefenEten extends Activity
     int arrayNum;
     int aantalWoorden;
     private String[] woorden = {
-            "Egel", "Ezel", "Geit",
-            "Hond","Jakhals","Kat",
-            "Kikker", "Kip", "Koe",
-            "Konijn", "Muis", "Paard",
-            "Schaap", "Vis", "Vogel"
+                  "Eieren", "olie", "koekjes",
+            "thee","koffie","melk",
+            "yoghurt", "water", "brood",
+            "honing", "suiker", "zout",
+            "vlees", "vissen", "boter"
+
     };
 
     private String[] vertaling = {
-            "Insi", "Aɣyul", "Tɣaṭṭ",
-            "Ayḍi","Uccen","Mucc",
-            "Aqaqriw", "Tyaziḍt", "Tafunast",
-            "Aqninni", "Aɣerda", "Ayis",
-            "Icerri", "Aslem", "Agḍiḍ"
+                      "timejjalin", "zzict", "ɣayiṭa",
+            "atay","rqehwa","aɣi",
+            "accil", "aman", "aɣrum",
+            "tamment", "sseqqur", "tamellaḥt",
+            "aysum", "iselman", "ddhen"
+
     };
 
     private int[] photos = {
-          R.drawable.dieren01_egel,R.drawable.dieren01_ezel, R.drawable.dieren01_geit,R.drawable.dieren01_hond, R.drawable.dieren01_jakhals, R.drawable.dieren01_kat,
-            R.drawable.dieren01_kikker, R.drawable.dieren01_kip, R.drawable.dieren01_koe, R.drawable.dieren01_konijn, R.drawable.dieren01_muis, R.drawable.dieren01_paard, R.drawable.dieren01_schaap, R.drawable.dieren01_vis, R.drawable.dieren01_vogel
-    };
+ R.drawableable.eten_eieren, R.drawableable.eten_olie,
+            R.drawableable.eten_koekjes, R.drawableable.eten_thee,
+            R.drawableable.eten_koffie, R.drawableable.eten_melk,
+            R.drawableable.eten_yoghurt, R.drawableable.eten_water,
+            R.drawableable.eten_brood, R.drawableable.eten_honing,
+            R.drawableable.eten_suiker, R.drawableable.eten_zout,
+            R.drawableable.eten_vlees, R.drawableable.eten_vissen,
+            R.drawableable.eten_boter    };
 
     private int[] geluid = {
-            R.raw.dieren01_egel,R.raw.dieren01_ezel, R.raw.dieren01_geit,R.raw.dieren01_hond, R.raw.dieren01_jakhals, R.raw.dieren01_kat,
-            R.raw.dieren01_kikker, R.raw.dieren01_kip, R.raw.dieren01_koe, R.raw.dieren01_konijn, R.raw.dieren01_muis, R.raw.dieren01_paard, R.raw.dieren01_schaap, R.raw.dieren01_vis, R.raw.dieren01_vogel
-    };
+ R.raw.eten_eieren, R.raw.eten_olie,
+            R.raw.eten_koekjes, R.raw.eten_thee,
+            R.raw.eten_koffie, R.raw.eten_melk,
+            R.raw.eten_yoghurt, R.raw.eten_water,
+            R.raw.eten_brood, R.raw.eten_honing,
+            R.raw.eten_suiker, R.raw.eten_zout,
+            R.raw.eten_vlees, R.raw.eten_vissen,
+            R.raw.eten_boter    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
