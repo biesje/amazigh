f98c0c58c345a3244fb10dc375b6f260083f7525
biejse <biestim7@gmail.com>
2016-06-29 22:56:31 +0200

    Insecten  werkt <3

--- app/src/main/java/com/example/timbiezeveld/amazighproj/QuizInsecten.java
+++ app/src/main/java/com/example/timbiezeveld/amazighproj/QuizInsecten.java
@@ -1,41 +1,32 @@
 package com.example.timbiezeveld.amazighproj;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 
 public class QuizInsecten extends AppCompatActivity {
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    ArrayList<Integer> mixlist = new ArrayList<Integer>();
 
-
-
-    private String[] vertaling = {
-            "aɣlal", "adan n tmurt", "takecca",
-            "iẓi","zizwit","nnamus",
-            "tikedfet", "aferṭṭu", "qundɣa ",
-            "burxes", "bugḍif", "yaẓẓa",
-            "buccel ", "tiḍḍa", "tamraebt"
-
-    };
-    private int[] photomix = {
-            R.drawable.insecten_slak, R.drawable.insecten_regenworm,
-            R.drawable.insecten_larve, R.drawable.insecten_vlieg,
-            R.drawable.insecten_bij, R.drawable.insecten_mug,
-            R.drawable.insecten_mier, R.drawable.insecten_vlinder,
-            R.drawable.insecten_spin, R.drawable.insecten_sprinkhaan,
-            R.drawable.insecten_rups, R.drawable.insecten_wesp,
-            R.drawable.insecten_naaktslak, R.drawable.insecten_bloedzuiger,
-            R.drawable.insecten_bidsprinkhaan
-    };
     private int[] photos = {
             R.drawable.insecten_slak, R.drawable.insecten_regenworm,
             R.drawable.insecten_larve, R.drawable.insecten_vlieg,
@@ -47,6 +38,14 @@ public class QuizInsecten extends AppCompatActivity {
             R.drawable.insecten_bidsprinkhaan
     };
 
+
+    private String[] vertaling = {
+            "aɣlal", "adan n tmurt", "takecca",
+            "iẓi","zizwit","nnamus",
+            "tikedfet", "aferṭṭu", "qundɣa ",
+            "burxes", "bugḍif", "yaẓẓa",
+            "buccel ", "tiḍḍa", "tamraebt"
+    };
     private int[] geluid = {
             R.raw.insecten_slak, R.raw.insecten_regenworm,
             R.raw.insecten_larve, R.raw.insecten_vlieg,
@@ -67,26 +66,25 @@ public class QuizInsecten extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        antPosition();
+        addTarr();
+
         quiznum = 0;
         score = 0;
+
+
+        antPosition();
+
+        loadImg();
         TextView amw = (TextView) findViewById(R.id.amazighwoord); // amazich woor afgekoort met amw
 
+
         amw.setText(vertaling[quiznum]);
-        loadImg();
-
-
-
-
-
-
-
 
 
     }
 
-    public void antPosition(){
-        int[] layouts = new int[] {R.layout.activity_quiz, R.layout.activity_quiz1,R.layout.activity_quiz2,R.layout.activity_quiz3,R.layout.activity_quiz4,R.layout.activity_quiz5,R.layout.activity_quiz6,};
+    public void antPosition() {
+        int[] layouts = new int[]{R.layout.activity_quiz, R.layout.activity_quiz1, R.layout.activity_quiz2, R.layout.activity_quiz3, R.layout.activity_quiz4, R.layout.activity_quiz5, R.layout.activity_quiz6,};
 
         setContentView(layouts[new Random().nextInt(layouts.length)]);
         setScore(0, true);
@@ -95,12 +93,37 @@ public class QuizInsecten extends AppCompatActivity {
 
     }
 
-    public void goedAntwoord(){
+    public void addTarr() {
+        list.clear();
+        list.add(R.drawable.insecten_slak);
+        list.add(R.drawable.insecten_regenworm);//
+        list.add(R.drawable.insecten_larve);
+        list.add(R.drawable.insecten_vlieg);
+        list.add(R.drawable.insecten_bij);
+        list.add(R.drawable.insecten_mug);
+        list.add(R.drawable.insecten_mier);
+        list.add(R.drawable.insecten_vlinder);
+        list.add(R.drawable.insecten_spin);
+        list.add(R.drawable.insecten_sprinkhaan);
+        list.add(R.drawable.insecten_rups);
+        list.add(R.drawable.insecten_wesp);
+        list.add(R.drawable.insecten_naaktslak);
+        list.add(R.drawable.insecten_bloedzuiger);
+        list.add(R.drawable.insecten_bidsprinkhaan);
+        list.remove(quiznum);
+
+    }
+
+    public void goedAntwoord() {
         setScore(2, false);
-        if(quiznum<vertaling.length-1) {
+        if (quiznum < vertaling.length - 1) {
             aantalfout = 0;
+
             quiznum++;
+            addTarr();
+
             antPosition();
+            loadImg();
 
             TextView amw = (TextView) findViewById(R.id.amazighwoord); // amazich woor afgekoort met amw
 
@@ -108,10 +131,8 @@ public class QuizInsecten extends AppCompatActivity {
             amw.setText(vertaling[quiznum]);
 
 
-            loadImg();
-        }
-        else{
-            Intent intent = new Intent(QuizInsecten.this,klaar.class);
+        } else {
+            Intent intent = new Intent(QuizInsecten.this, klaar.class);
             intent.putExtra("extra_text", String.valueOf(this.score));
 
             startActivity(intent);
@@ -122,7 +143,7 @@ public class QuizInsecten extends AppCompatActivity {
     }
 
 
-    public void setFout(int i, String k){
+    public void setFout(int i, String k) {
         switch (i) {
             case 1:
                 Button btn1 = (Button) findViewById(R.id.photo1); // amazich woor afgekoort met amw
@@ -149,21 +170,19 @@ public class QuizInsecten extends AppCompatActivity {
                 break;
             case 6:
                 Button btn6 = (Button) findViewById(R.id.photo6); // amazich woor afgekoort met amw
-                shuffle();
                 btn6.setBackgroundResource(R.drawable.kruis);
 
 
+        }
+    }
 
-
-        }}
-
-    public void foutAntwoord(int i){
-        setFout(i,"#FF0000" );
+    public void foutAntwoord(int i) {
+        setFout(i, "#FF0000");
         setScore(-1, false);
     }
 
 
-    public void clickAntwoord(View v){
+    public void clickAntwoord(View v) {
         switch (v.getId()) {
             case R.id.photo1:
 
@@ -189,9 +208,9 @@ public class QuizInsecten extends AppCompatActivity {
         aantalfout++;
 
 
-
     }
-    public void loadImg(){
+
+    public void loadImg() {
         Random rand = new Random();
 
         int n2;
@@ -202,10 +221,9 @@ public class QuizInsecten extends AppCompatActivity {
         int n6;
 
 
-
         n2 = 1;
 
-        n3 =  2;
+        n3 = 2;
         n4 = 3;
 
         n5 = 4;
@@ -213,79 +231,36 @@ public class QuizInsecten extends AppCompatActivity {
         n6 = 5;
 
 
-        RandomizeArray(photomix);
         Button btn1 = (Button) findViewById(R.id.photo1); // amazich woor afgekoort met amw
         btn1.setBackgroundResource(photos[quiznum]);
 
         Button btn2 = (Button) findViewById(R.id.photo2); // amazich woor afgekoort met amw
-        btn2.setBackgroundResource(photomix[2]);
+        btn2.setBackgroundResource(list.get(1));
         Button btn3 = (Button) findViewById(R.id.photo3); // amazich woor afgekoort met amw
-        btn3.setBackgroundResource(photomix[3]);
+        btn3.setBackgroundResource(list.get(4));
         Button btn4 = (Button) findViewById(R.id.photo4); // amazich woor afgekoort met amw
-        btn4.setBackgroundResource(photomix[5]);
+        btn4.setBackgroundResource(list.get(7));
         Button btn5 = (Button) findViewById(R.id.photo5); // amazich woor afgekoort met amw
-        btn5.setBackgroundResource(photomix[6]);
+        btn5.setBackgroundResource(list.get(5));
         Button btn6 = (Button) findViewById(R.id.photo6); // amazich woor afgekoort met amw
-        btn6.setBackgroundResource(photomix[7]);
+        btn6.setBackgroundResource(list.get(9));
     }
 
 
+    public void setScore(int score, boolean aanpas) {
 
-
-
-
-    public static void RandomizeArray(int[] array){
-        Random rgen = new Random();  // Random number generator
-
-        if(array[1]==1){
-
-        }
-
-        for (int i=0; i<array.length; i++) {
-            int randomPosition = rgen.nextInt(array.length);
-            int temp = array[i];
-
-            array[i] = array[randomPosition];
-            array[randomPosition] = temp;
-        }
-
-    }
-    public void setScore(int score, boolean aanpas){
-
-        this.score=this.score+score;
+        this.score = this.score + score;
         TextView txt = (TextView) findViewById(R.id.score); // score view
-        if(aanpas) {
+        if (aanpas) {
 
             txt.setText("Score: " + String.valueOf(this.score));
         }
     }
 
-    public void shuffle(){
-        List<Integer> objects = new ArrayList<Integer>();
-        objects.add(0);
-        objects.add(1);
-        objects.add(2);
-        objects.add(3);
-        objects.add(4);
-
-        // Shuffle the collection
-        Collections.shuffle(objects);
-
-        List<Button> buttons = new ArrayList<Button>();
-        buttons.add((Button)findViewById(R.id.photo1));
-        buttons.add((Button)findViewById(R.id.photo2));
-        buttons.add((Button)findViewById(R.id.photo4));
-        buttons.add((Button)findViewById(R.id.photo5));
-        buttons.add((Button)findViewById(R.id.photo6));
-
-        for (int i = 0; i < objects.size(); i++) {
-            buttons.get(i).setText(objects.get(i).toString());
-        }
-
-
-
-    }
-
-
 
 }
+
+
+
+
+
