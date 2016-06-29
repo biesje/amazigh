841d1a714a18a8b25a6c7a3ccaf0520fd26f8eec
biejse <biestim7@gmail.com>
2016-06-29 17:31:45 +0200

    Catogory toegevoegd

--- app/src/main/java/com/example/timbiezeveld/amazighproj/CatogoryQuiz.java
+++ app/src/main/java/com/example/timbiezeveld/amazighproj/CatogoryQuiz.java
@@ -1,12 +1,11 @@
 package com.example.timbiezeveld.amazighproj;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class Catogory extends AppCompatActivity {
+public class CatogoryQuiz extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,37 +16,37 @@ public class Catogory extends AppCompatActivity {
 
     public void startDieren1(View v){
 
-        Intent intent1 = new Intent(Catogory.this,OefenDieren1.class);
+        Intent intent1 = new Intent(CatogoryQuiz.this,OefenDieren1.class);
         startActivity(intent1);
 
 
     }
     public void startEten(View v){
 
-       Intent intent2 = new Intent(Catogory.this,OefenFood.class);
+       Intent intent2 = new Intent(CatogoryQuiz.this,QuizEten.class);
         startActivity(intent2);
 
 
     }
     public void oefenFruit(View v){
 
-        Intent intent3 = new Intent(Catogory.this,OefenFruit.class);
+        Intent intent3 = new Intent(CatogoryQuiz.this,QuizFruit.class);
         startActivity(intent3);
 
 
     }
-    
+
         public void oefenWeer(View v){
 
-        Intent intent3 = new Intent(Catogory.this,OefenWeer.class);
+        Intent intent3 = new Intent(CatogoryQuiz.this,QuizWeer.class);
         startActivity(intent3);
 
 
     }
-    
+
         public void oefenDieren1(View v){
 
-        Intent intent3 = new Intent(Catogory.this,OefenDieren1.class);
+        Intent intent3 = new Intent(CatogoryQuiz.this,QuizDieren1.class);
         startActivity(intent3);
 
 
@@ -55,48 +54,48 @@ public class Catogory extends AppCompatActivity {
 
         public void oefenDieren2(View v){
 
-        Intent intent3 = new Intent(Catogory.this,OefenDieren2.class);
+        Intent intent3 = new Intent(CatogoryQuiz.this,QuizDieren2.class);
         startActivity(intent3);
 
 
     }
-    
+
         public void oefenEten(View v){
 
-        Intent intent3 = new Intent(Catogory.this,OefenEten.class);
+        Intent intent3 = new Intent(CatogoryQuiz.this,QuizEten.class);
         startActivity(intent3);
 
 
     }
-    
+
         public void oefenGroente(View v){
 
-        Intent intent3 = new Intent(Catogory.this,OefenGroente.class);
+        Intent intent3 = new Intent(CatogoryQuiz.this,QuizGroente.class);
         startActivity(intent3);
 
 
     }
-    
+
         public void oefenInsecten(View v){
 
-        Intent intent3 = new Intent(Catogory.this,OefenInsecten.class);
+        Intent intent3 = new Intent(CatogoryQuiz.this,QuizInsecten.class);
         startActivity(intent3);
 
 
     }
-    
-    
+
+
         public void oefenKleding(View v){
 
-        Intent intent3 = new Intent(Catogory.this,OefenKleding.class);
+        Intent intent3 = new Intent(CatogoryQuiz.this,QuizKleding.class);
         startActivity(intent3);
 
 
     }
-    
+
         public void oefenKleur(View v){
 
-        Intent intent3 = new Intent(Catogory.this,OefenKleur.class);
+        Intent intent3 = new Intent(CatogoryQuiz.this,QuizKleur.class);
         startActivity(intent3);
 
 
