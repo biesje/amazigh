e1126dd3e6ab373444eb2b23289069e6defeed26
justin0077 <justinkwaak@hotmail.com>
2016-06-29 08:58:03 +0200

    Update Catogory.java

--- app/src/main/java/com/example/timbiezeveld/amazighproj/Catogory.java
+++ app/src/main/java/com/example/timbiezeveld/amazighproj/Catogory.java
@@ -36,5 +36,74 @@ public class Catogory extends AppCompatActivity {
 
 
     }
+    
+        public void oefenWeer(View v){
 
+        Intent intent3 = new Intent(Catogory.this,OefenWeer.class);
+        startActivity(intent3);
+
+
+    }
+    
+        public void oefenDieren1(View v){
+
+        Intent intent3 = new Intent(Catogory.this,OefenDieren1.class);
+        startActivity(intent3);
+
+
+    }
+
+        public void oefenDieren2(View v){
+
+        Intent intent3 = new Intent(Catogory.this,OefenDieren2.class);
+        startActivity(intent3);
+
+
+    }
+    
+        public void oefenEten(View v){
+
+        Intent intent3 = new Intent(Catogory.this,OefenEten.class);
+        startActivity(intent3);
+
+
+    }
+    
+        public void oefenGroente(View v){
+
+        Intent intent3 = new Intent(Catogory.this,OefenGroente.class);
+        startActivity(intent3);
+
+
+    }
+    
+        public void oefenInsecten(View v){
+
+        Intent intent3 = new Intent(Catogory.this,OefenInsecten.class);
+        startActivity(intent3);
+
+
+    }
+    
+    
+        public void oefenKleding(View v){
+
+        Intent intent3 = new Intent(Catogory.this,OefenKleding.class);
+        startActivity(intent3);
+
+
+    }
+    
+        public void oefenKleur(View v){
+
+        Intent intent3 = new Intent(Catogory.this,OefenKleur.class);
+        startActivity(intent3);
+
+
+    }
+    
+    
+    
+    
+    
 }
